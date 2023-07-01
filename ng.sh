74b0d506ccc9fb0a5b4464824f9858a4e507fa88
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-01 15:15:43 +0700

    Create ng.sh

---
+++ ng.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Download Ngrok"
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
+unzip ngrok.zip > /dev/null 2>&1
+sleep 2
+
+NGROK_AUTH_TOKEN="2RIDE4oxtvtFSHR9Ea1dI3BZnZo_4hoNaQK6cLt3j3WHpjc9E"
+./ngrok authtoken $NGROK_AUTH_TOKEN
+sleep 2
+./ngrok tcp --region ap 22 
+
+echo "Ngrok setup completed"
+echo "========="
+#close
