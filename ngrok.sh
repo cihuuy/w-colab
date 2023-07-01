62a931f6f5aaa070cbc71625053d4c8b8aa7d992
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-07-01 14:31:57 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "download ngrok"
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+echo "Download Ngrok"
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
 unzip ngrok.zip > /dev/null 2>&1
 sleep 2
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP 
-nohup ./ngrok tcp --region ap 3389 &>/dev/null &
-echo "======================="
+
+NGROK_AUTH_TOKEN="2RIDE4oxtvtFSHR9Ea1dI3BZnZo_4hoNaQK6cLt3j3WHpjc9E"
+./ngrok authtoken $NGROK_AUTH_TOKEN
+
+nohup ./ngrok tcp --region ap 3389 
+
+echo "Ngrok setup completed"
+echo "========="
 
 #close
